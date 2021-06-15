a587673e90bb0fd145fa352a8514ab75e2c06b3a
Hiroshi <32210429+hiroshi-nz@users.noreply.github.com>
2021-06-15 14:45:21 +1200

    Add files via upload

---
+++ Class/Connection/Bolt/BoltData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CivilApp.Class.Connection.Bolt
+{
+    class BoltData
+    {
+        public string name { get; set; }
+        public double As;//Tensile area
+        public double Ao;//Shank area
+        public double Ac;//Core area
+
+        public BoltData(string name, double As, double Ao, double Ac)
+        {
+            this.name = name;
+            this.As = As;
+            this.Ao = Ao;
+            this.Ac = Ac;
+        }
+
+    }
+}
